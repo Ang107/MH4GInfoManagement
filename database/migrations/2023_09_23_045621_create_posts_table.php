441e474ae502e0ecaeabcde23945704261385261
Ang107 <kosukenabeta1007@gmail.com>
2023-09-23 15:07:54 +0900

    migration作成

---
+++ database/migrations/2023_09_23_045621_create_posts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); 
+            $table->integer('default_level');
+            $table->foreignId('right_monster_id')->constrained('monsters'); 
+            $table->integer('right_monster_area');
+            $table->foreignId('left_monster_id')->constrained('monsters'); 
+            $table->integer('left_monster_area');
+            $table->foreignId('area_1_id')->constrained('areas'); 
+            $table->foreignId('area_2_id')->constrained('areas'); 
+            $table->foreignId('area_3_id')->constrained('areas'); 
+            $table->foreignId('area_4_id')->constrained('areas'); 
+            $table->integer('treasure_area_number');
+            $table->foreignId('weapon_id')->constrained(); 
+            $table->foreignId('armor_id')->constrained(); 
+            $table->foreignId('armor_port_id')->constrained(); 
+            $table->string('generator',20);
+            $table->integer('bookmark_number');
+            $table->string('remark',200);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_09_23_054703_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained(); 
+            $table->foreignId('room_id')->constrained(); 
+            $table->string('body',400);
+            $table->timestamp('sent_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
